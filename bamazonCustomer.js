9cf4aa47d0827db7a3a549067efdcf36b8ad6bf9
clarymt <clarymt@me.com>
2019-10-28 23:18:22 -0400

    result is not defined....................................................................................................................................................................................................................................................................................................

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,30 +23,30 @@ connection.connect(function(err) {
 
 function runSearch() {
     console.log("Selecting all products...\n");
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function(err, results) {
       //similar code when comparing inquirer results and DB
       if (err) throw err;
       // Log all results of the SELECT statement
-      for (var i = 0; i < res.length; i++) {
-        console.log(res[i].item_id);
-        console.log(res[i].product_name);
-        console.log(res[i].department_name);
-        console.log(res[i].price);
-        console.log(res[i].stock_quantity);
+      for (var i = 0; i < results.length; i++) {
+        console.log(results[i].item_id);
+        console.log(results[i].product_name);
+        console.log(results[i].department_name);
+        console.log(results[i].price);
+        console.log(results[i].stock_quantity);
         console.log("\n");
       }
       //to be used after no longer searchign database connection.end()
-      rangeSearch();
+      itemChoice();
       
     });
-  }
+  
 
-  function rangeSearch() {
+  function itemChoice() {
     inquirer
       .prompt([
         {
           name: "userPurchase",
-          type: "input",
+          type: "rawlist",
           message: "Which product would you like to purchase? (Enter Id_#) ",
      
         },
@@ -54,13 +54,58 @@ function runSearch() {
           name: "quantity",
           type: "input",
           message: "How many would you like to purchase? ",
+      
          
         }
       ])
       
       .then(function(answer) {
-        console.log(answer.userPurchase);
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].item_name === answer.userPurchase) {
+            chosenItem = results[i];
+          }
+        }
+
+        // determine if bid was high enough
+        if (chosenItem.stock_quantity < parseInt(answer.quantity)) {
+          // bid was high enough, so update db, let the user know, and start over
+          connection.query(
+            "UPDATE auctions SET ? WHERE ?",
+            [
+              {
+                stock_quantity: stock_quantity - answer.quantity
+              },
+              {
+                item_id: chosenItem.item_id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Bid placed successfully!");
+              start();
+            }
+          );
+        }
+        else {
+          // bid wasn't high enough, so apologize and start over
+          console.log("Your bid was too low. Try again...");
+          runSearch();
+        }
+      });
+          /*var itemSelected = answer.userPurchase;
+          var quantityChosen = answer.quantity;
+           console.log(itemSelected);
         console.log(answer.quantity);
+        for (var i = 0; i < results.length; i++){
+          if (quantityChosen > results[itemSelected].stock_quantity) {
+              console.log("We simply cannot supply your needs at this moment in time.")
+          }
+          else {
+            console.log("Thanks for your business! You have purchased " + answer.quantity +" " + itemSelected + "(s)")
+          }}*/
+          /*
+       
         var query = "SELECT product_name, stock_quantity FROM bamazon WHERE item_id=answer.userPurchase";
       connection.query(query, [answer.userPurchase, answer.quantity], function(err, res) {
           console.log(
@@ -68,10 +113,10 @@ function runSearch() {
           );
         
         //runSearch();
-      });
-    });
+          */ });
+    
 
-}
+}}
         
         
 
